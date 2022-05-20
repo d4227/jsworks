72e69e3f2ef8bcaf1274ba6df53f23c5ad39071b
d4227 <d4227@naver.com>
2022-05-20 18:18:47 +0900

    day10 자바스크립트 소스

---
+++ day10/js/calc.js
@@ -0,0 +1,11 @@
+// 덧셈 프로그램
+let n1 = prompt("첫번째 수 입력 : ");
+let n2 = prompt("두번째 수 입력 : ");
+let sum_v = 0;
+
+// 연산
+// 입력받은 문자를 정수로 변환함.
+sum_v = parseInt(n1) + parseInt(n2);
+
+//출력
+document.write("두 수의 합은 : " + sum_v);1
